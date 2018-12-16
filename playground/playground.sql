195dac95640e3fcdab7b4a5997c8498bd269b685
gituserDoesNotExist <manfred-beutel@web.de>
2018-12-16 11:43:10 +0100

    cloning

---
+++ playground/playground.sql
@@ -0,0 +1,24 @@
+﻿
+DECLARE
+  SHIT_1        SHIT        := NEW SHIT('hello');
+
+  TEST_OBJECT_1 TEST_OBJECT := NEW TEST_OBJECT(SHIT_1);
+
+  TMP_SHIT      SHIT;
+BEGIN
+  DBMS_OUTPUT.PUT_LINE('initial value of shit1: ' || SHIT_1.SOME_TEXT);
+  SHIT_1.SET_SOME_TEXT('new text');
+  DBMS_OUTPUT.PUT_LINE('new value set for shit1: ' || SHIT_1.SOME_TEXT);
+
+  DBMS_OUTPUT.PUT_LINE('value of shit1 in test-object-1: ' || TEST_OBJECT_1.GET_SOME_SHIT().SOME_TEXT);
+  DBMS_OUTPUT.PUT_LINE('value of shit1 in test-object-1: ' || TEST_OBJECT_1.SOME_SHIT.SOME_TEXT);
+
+
+  TMP_SHIT := TEST_OBJECT_1.GET_SOME_SHIT();
+  DBMS_OUTPUT.PUT_LINE('assigned shit1 to tmp-shit. value of tmp-shit: ' || TMP_SHIT.SOME_TEXT);
+  TMP_SHIT.SET_SOME_TEXT('new tmp shit value');
+  DBMS_OUTPUT.PUT_LINE('changed value of tmp-shit. new value: ' || TMP_SHIT.SOME_TEXT);
+
+  DBMS_OUTPUT.PUT_LINE('shit value of test-object-1 is ' || TEST_OBJECT_1.GET_SOME_SHIT().SOME_TEXT);
+
+END;
---
+++ variable-assignment-clones-object.sql
@@ -0,0 +1,17 @@
+﻿create or replace type cla as object    -- class (would be very complex)
+(
+  name  varchar2(50)
+);
+/
+
+declare
+    o1 cla;
+    o2 cla;
+begin
+    o1 := cla('hi cloning world');
+    o2 := o1;
+    o1.name := 'goodbye cloning world';
+
+    dbms_output.put_line('o1.name: ' || o1.name);
+    dbms_output.put_line('o2.name: ' || o2.name);
+end;
